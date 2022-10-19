3cb6447f389cb237a099f5b1483a518ed3c63f68
daniatoudjibril <daniatoudjibril@gmail.com>
2022-10-19 23:34:12 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './App.css';
 import Header from './Header';
 import Main from './Main'
-
-
 
 
 class App extends React.Component {
